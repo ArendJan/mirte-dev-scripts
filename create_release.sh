5cc703d819f8d1034491d7751f095057d8e5d5e6
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-12-05 11:56:34 +0100

    add known issues text option release

--- create_release.sh
+++ create_release.sh
@@ -9,13 +9,13 @@ set -o pipefail
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_DIR=$(realpath "$SCRIPT_DIR")
 
-release_name="v0.2.0-rc.1"
+release_name="v0.2.0-rc.2"
 latest=false
 # read repos.yaml and get the list of packages
+repos_file="$SCRIPT_DIR/repos_dev.yaml"
 repos_file="$SCRIPT_DIR/repos.yaml"
-
 if [ ! -f "$repos_file" ]; then
-    echo "repos.yaml not found!"
+    echo "$repos_file not found!"
     exit 1
 fi
 
@@ -30,15 +30,21 @@ for package in $packages; do
     branch=$(echo "$branches" | sed -n "${i}p")
     i=$((i + 1))
     echo "Processing $package on branch $branch"
-    # gh release --repo "$package" create "$release_name" --target "$branch" --title "$release_name" --generate-notes --latest="$latest" || true
+    gh release --repo "$package" create "$release_name" --target "$branch" --title "$release_name" --generate-notes --latest="$latest" || true
     notes=$(gh release --repo "$package" view "$release_name" --json body -q .body)
     all_notes+="## Release notes for $(basename "$package")\n\n"
     all_notes+="$notes\n\n"
 done
 # exit 0
 
+known_issues_file="$SCRIPT_DIR/known_issues.md"
+known_issues=""
+if [ -f "$known_issues_file" ]; then
+    known_issues="$(cat "$known_issues_file")"
+fi
+
 # add download link to the top of the notes
-all_notes="### [Download the latest sd-image](https://surfdrive.surf.nl/s/193nJP6OkYzHds0)\n\n$all_notes"
+all_notes="### [Download the latest sd-image](https://surfdrive.surf.nl/s/193nJP6OkYzHds0?dir=$release_name)\n\n$known_issues\n\n$all_notes"
 
 # write all notes to a file
 echo -e "$all_notes" > "$SCRIPT_DIR/all_release_notes.md"
---
+++ known_issues.md
@@ -0,0 +1,3 @@
+# Known Issues:
+- uploading firmware to the Pico over UART might fail the first time, second try should work.
+  - Updating the Pico is required as there are some small protocol changes!
